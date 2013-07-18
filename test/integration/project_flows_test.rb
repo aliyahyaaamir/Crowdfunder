0fcf20214511015aee71e8790ae7747085295561
ali aamir <ali_aamir@hotmail.com>
2013-07-18 18:10:07 -0400

    Writing tests for browsing projects

---
+++ test/integration/project_flows_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ProjectFlowsTest < ActionDispatch::IntegrationTest
+
+ test "browsing projects" do
+	project1 = FactoryGirl.create(:project, :title => "Project1")
+	project2 = FactoryGirl.create(:project, :title => "Project 2")
+	project3 = FactoryGirl.create(:project, :title => "Project 3")
+
+	visit "/projects"
+
+	assert_equal projects_path, current_path
+
+	assert page.has_content?('Listing projects')
+
+
+	assert page.has_content?('Project 1')
+	assert page.has_content?('Project 2')
+	assert page.has_content?('Project 3')
+
+ end
+
+end
